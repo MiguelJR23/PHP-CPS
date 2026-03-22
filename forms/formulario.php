220d6073e7d184514092e50e37b5dca08de58b1a
MiguelJR23 <r0b3rts2332@gmail.com>
2026-03-22 16:25:48 -0300

    Melhorando o forms

--- forms/formulario.php
+++ forms/formulario.php
@@ -2,7 +2,7 @@
 <main>
     <section class="contato">
         <div class="container">
-            <h1>Formulário</h1>
+            <h1>Agende uma visita!</h1>
 
             <form action="processar.php" method="POST">
                 
@@ -13,6 +13,31 @@
 
                 <p>Sua idade:</p>
                 <input type="text" name="idade">
+
+                <br><br>
+                  
+                <p>Motivo da visita:</p>
+                <input type="text" name="motivo">
+
+                <br><br>
+                  
+                <p>CPF:</p>
+                <input type="text" name="cpf">
+
+                <br><br>
+                  
+                <p>Número de telefone:</p>
+                <input type="text" name="telefone">
+
+                <br><br>
+                  
+                <p>Email:</p>
+                <input type="text" name="email">
+
+                <br><br>
+                  
+                <p>Endereço:</p>
+                <input type="text" name="endereco">
 
                 <br><br>
 
--- forms/processar.php
+++ forms/processar.php
@@ -1,21 +1,58 @@
 <?php include '../includes/header.php'; ?>
+
 <main>
     <section class="contato">
         <div class="container">
-            <h1>Dados recebidos</h1>
 
-            <p>
-                <?php echo $_POST["nome"]; ?>
-            </p>
+            <?php if(isset($_POST["confirmado"])) { ?>
 
-            <p>
-                Você tem <?php echo $_POST["idade"]; ?> anos.
-            </p>
+                <h1>Obrigado!</h1>
 
-            <br>
+                <div style="text-align: center;">
+                    <img src="../assets/img/checkmark.png" alt="Sucesso" style="width: 100px; height: auto;">
 
-            <a href="../index.php" class="btn">Voltar</a>
+                    <p>
+                        Sua solicitação foi enviada com sucesso.
+                    </p>
+                </div>
+
+                <p>
+                    Iremos enviar uma mensagem por email confirmando a data da visita.
+                </p>
+
+                <br>
+
+                <a href="../index.php" class="btn">Voltar ao início</a>
+
+            <?php } else { ?>
+
+                <h1>Confirme seus dados</h1>
+
+                <p><strong>Nome:</strong> <?php echo $_POST["nome"]; ?></p>
+                <p><strong>Idade:</strong> <?php echo $_POST["idade"]; ?> anos</p>
+                <p><strong>Motivo da visita:</strong> <?php echo $_POST["motivo"]; ?></p>
+                <p><strong>CPF:</strong> <?php echo $_POST["cpf"]; ?></p>
+                <p><strong>Telefone:</strong> <?php echo $_POST["telefone"]; ?></p>
+                <p><strong>Email:</strong> <?php echo $_POST["email"]; ?></p>
+                <p><strong>Endereço:</strong> <?php echo $_POST["endereco"]; ?></p>
+
+                <br>
+
+                <p><strong>Você confirma que seus dados estão corretos?</strong></p>
+
+                <form method="POST">
+                    <input type="hidden" name="confirmado" value="sim">
+                    <input type="submit" value="Confirmar" class="btn">
+                </form>
+
+                <br>
+
+                <a href="formulario.php" class="btn">Voltar</a>
+
+            <?php } ?>
+
         </div>
     </section>
 </main>
+
 <?php include '../includes/footer.php'; ?>
