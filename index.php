b3fc7c81bccef954c7945463c43de8eec9569393
MiguelJR23 <r0b3rts2332@gmail.com>
2026-03-22 15:35:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
         <h2>Nossos Cursos</h2>
         <div class="cards">
             <div class="card">
-                <img src="assets/img/placeholder.jpg" alt="Curso">
+                <img src="assets/img/placeholder.png" alt="Curso">
                 <h3>Desenvolvimento de Sistemas</h3>
                 <p>Aprenda lógica, programação e criação de sistemas.</p>
                 <a href="pages/cursos.php" class="btn-card">Saiba mais</a>
             </div>
 
             <div class="card">
-                <img src="assets/img/placeholder.jpg" alt="Curso">
+                <img src="assets/img/placeholder.png" alt="Curso">
                 <h3>Administração</h3>
                 <p>Gestão empresarial, finanças e organização.</p>
                 <a href="pages/cursos.php" class="btn-card">Saiba mais</a>
             </div>
 
             <div class="card">
-                <img src="assets/img/placeholder.jpg" alt="Curso">
+                <img src="assets/img/placeholder.png" alt="Curso">
                 <h3>Logística</h3>
                 <p>Estratégias de otimização de produtos e informações.</p>
                 <a href="pages/cursos.php" class="btn-card">Saiba mais</a>
